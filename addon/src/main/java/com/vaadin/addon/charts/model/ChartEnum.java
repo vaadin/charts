b591318177cee8a9e063d34308d3d6fbcc29e8b0
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-11-13 16:33:29 +0000

    re fixing serializable
    git-svn-id: http://dev.vaadin.com/svn/addons/vaadin-charts@25039 de684a11-831f-0410-ab27-cb86445eb682

--- addon/src/main/java/com/vaadin/addon/charts/model/ChartEnum.java
+++ addon/src/main/java/com/vaadin/addon/charts/model/ChartEnum.java
@@ -1,9 +1,11 @@
 package com.vaadin.addon.charts.model;
 
+import java.io.Serializable;
+
 /**
  * Interface shared by all enum classes of the vaadin charts. It is used when
  * serializing enums to gson
  */
-public interface ChartEnum {
+public interface ChartEnum extends Serializable {
 
 }
