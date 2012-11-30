52f7c6edbec0d57b6bd457db20a672414a402dae
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-11-30 09:17:03 +0000

    added required method
    git-svn-id: http://dev.vaadin.com/svn/addons/vaadin-charts@25135 de684a11-831f-0410-ab27-cb86445eb682

--- addon/src/main/java/com/vaadin/addon/charts/client/ui/ThemerConnector.java
+++ addon/src/main/java/com/vaadin/addon/charts/client/ui/ThemerConnector.java
@@ -2,6 +2,7 @@ package com.vaadin.addon.charts.client.ui;
 
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.json.client.JSONParser;
+import com.vaadin.client.ServerConnector;
 import com.vaadin.client.VConsole;
 import com.vaadin.client.communication.StateChangeEvent;
 import com.vaadin.client.extensions.AbstractExtensionConnector;
@@ -31,5 +32,10 @@ public class ThemerConnector extends AbstractExtensionConnector {
     @Override
     public ThemerState getState() {
         return (ThemerState) super.getState();
+    }
+
+    @Override
+    protected void extend(ServerConnector target) {
+        // NOP
     }
 }
