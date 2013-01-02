d568c6b96497a5ba2743c6f88ff0fdb7b76b8219
Jonatan Kronqvist <jonatan.kronqvist@itmill.com>
2013-01-02 11:05:39 +0000

    Cleaned up the PieWithGradientFill example a bit
    git-svn-id: http://dev.vaadin.com/svn/addons/vaadin-charts@25247 de684a11-831f-0410-ab27-cb86445eb682

--- addon/src/test/java/com/vaadin/addon/charts/demoandtestapp/pie/PieWithGradientFill.java
+++ addon/src/test/java/com/vaadin/addon/charts/demoandtestapp/pie/PieWithGradientFill.java
@@ -40,46 +40,46 @@ public class PieWithGradientFill extends AbstractVaadinChartExample {
         plotOptions.setDataLabels(dataLabels);
         conf.setPlotOptions(plotOptions);
 
-        DataSeries series = new DataSeries();
-        DataSeriesItem item = new DataSeriesItem("Firefox", 45.0);
-        item.setColor(getGradientColor(new SolidColor(255, 128, 0),
-                new SolidColor(128, 64, 0)));
-        series.addData(item);
+        conf.setSeries(getBrowserMarketShareSeries());
 
-        item = new DataSeriesItem("IE", 26.8);
-        item.setColor(getGradientColor(new SolidColor(0, 255, 255),
-                new SolidColor(0, 128, 128)));
-        series.addData(item);
-
-        DataSeriesItem chrome = new DataSeriesItem("Chrome", 12.8);
-        chrome.setColor(getGradientColor(new SolidColor(255, 255, 0),
-                new SolidColor(128, 128, 0)));
-        chrome.setSliced(true);
-        chrome.setSelected(true);
-        series.addData(chrome);
-
-        item = new DataSeriesItem("Safari", 8.5);
-        item.setColor(getGradientColor(new SolidColor(0, 128, 255),
-                new SolidColor(0, 64, 128)));
-        series.addData(item);
-
-        item = new DataSeriesItem("Opera", 6.2);
-        item.setColor(getGradientColor(new SolidColor(255, 0, 0),
-                new SolidColor(128, 0, 0)));
-        series.addData(item);
-
-        item = new DataSeriesItem("Others", 0.7);
-        item.setColor(getGradientColor(new SolidColor(0, 128, 0),
-                new SolidColor(0, 64, 0)));
-        series.addData(item);
-
-        conf.setSeries(series);
-
-        chart.drawChart(conf);
+        chart.drawChart();
         return chart;
     }
 
-    private GradientColor getGradientColor(SolidColor start, SolidColor end) {
+    private DataSeries getBrowserMarketShareSeries() {
+        DataSeriesItem firefox = new DataSeriesItem("Firefox", 45.0);
+        firefox.setColor(createRadialGradient(new SolidColor(255, 128, 0),
+                new SolidColor(128, 64, 0)));
+
+        DataSeriesItem ie = new DataSeriesItem("IE", 26.8);
+        ie.setColor(createRadialGradient(new SolidColor(0, 255, 255),
+                new SolidColor(0, 128, 128)));
+
+        DataSeriesItem chrome = new DataSeriesItem("Chrome", 12.8);
+        chrome.setColor(createRadialGradient(new SolidColor(255, 255, 0),
+                new SolidColor(128, 128, 0)));
+        chrome.setSliced(true);
+        chrome.setSelected(true);
+
+        DataSeriesItem safari = new DataSeriesItem("Safari", 8.5);
+        safari.setColor(createRadialGradient(new SolidColor(0, 128, 255),
+                new SolidColor(0, 64, 128)));
+
+        DataSeriesItem opera = new DataSeriesItem("Opera", 6.2);
+        opera.setColor(createRadialGradient(new SolidColor(255, 0, 0),
+                new SolidColor(128, 0, 0)));
+
+        DataSeriesItem others = new DataSeriesItem("Others", 0.7);
+        others.setColor(createRadialGradient(new SolidColor(0, 128, 0),
+                new SolidColor(0, 64, 0)));
+
+        return new DataSeries(firefox, ie, chrome, safari, opera, others);
+    }
+
+    /**
+     * Creates a radial gradient with the specified start and end colors.
+     */
+    private GradientColor createRadialGradient(SolidColor start, SolidColor end) {
         GradientColor color = GradientColor.createRadial(0.5, 0.3, 0.7);
         color.addColorStop(0, start);
         color.addColorStop(1, end);
