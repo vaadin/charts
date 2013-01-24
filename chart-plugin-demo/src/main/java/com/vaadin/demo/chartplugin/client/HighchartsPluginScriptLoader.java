ca75410d8fc37b546bdcff919d7e1c2c4bc57ca3
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-01-24 12:55:19 +0000

    Default Vaadin Charts theme again applied as last, otherwise breaks types from the "more" package
    git-svn-id: http://dev.vaadin.com/svn/addons/vaadin-charts@25394 de684a11-831f-0410-ab27-cb86445eb682

--- chart-plugin-demo/src/main/java/com/vaadin/demo/chartplugin/client/HighchartsPluginScriptLoader.java
+++ chart-plugin-demo/src/main/java/com/vaadin/demo/chartplugin/client/HighchartsPluginScriptLoader.java
@@ -33,8 +33,8 @@ public class HighchartsPluginScriptLoader extends HighchartsScriptLoader {
         inject(HighchartPluginResources.INSTANCE.worldMapShapes().getText());
         inject(HighchartPluginResources.INSTANCE.mapSrcJs().getText());
 
-        inject(HighchartResources.INSTANCE.defaultTheme().getText());
         inject(HighchartResources.INSTANCE.highchartsMore().getText());
         inject(HighchartResources.INSTANCE.exporting().getText());
+        inject(HighchartResources.INSTANCE.defaultTheme().getText());
     }
 }
