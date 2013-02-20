0b5f1448d7b3945b3f4169b7d8911b514cd7244a
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-02-20 14:42:13 +0000

    test for #11091
    git-svn-id: http://dev.vaadin.com/svn/addons/vaadin-charts@25584 de684a11-831f-0410-ab27-cb86445eb682

---
+++ addon/src/test/java/com/vaadin/addon/charts/testbenchtests/BasicBarNoTitleTBTest.java
@@ -0,0 +1,17 @@
+package com.vaadin.addon.charts.testbenchtests;
+
+import com.vaadin.addon.charts.demoandtestapp.columnandbar.BasicBarNoTitle;
+
+public class BasicBarNoTitleTBTest extends AbstractSimpleScreenShotTestBenchTest {
+
+    @Override
+    protected String getTestViewName() {
+        return BasicBarNoTitle.class.getSimpleName();
+    }
+
+    @Override
+    protected String getPackageName() {
+        return "columnandbar";
+    }
+
+}
