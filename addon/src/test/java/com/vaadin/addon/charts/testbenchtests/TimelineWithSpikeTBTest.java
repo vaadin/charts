578b566f4a481fd756247f013e956b7109bcb657
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-03-22 12:47:09 +0000

    Added automated screenshot test for timeline advanced point reducing algorithm test
    git-svn-id: http://dev.vaadin.com/svn/addons/vaadin-charts@25730 de684a11-831f-0410-ab27-cb86445eb682

---
+++ addon/src/test/java/com/vaadin/addon/charts/testbenchtests/TimelineWithSpikeTBTest.java
@@ -0,0 +1,12 @@
+package com.vaadin.addon.charts.testbenchtests;
+
+import com.vaadin.addon.charts.demoandtestapp.timeline.TimelineWithSpike;
+
+public class TimelineWithSpikeTBTest extends
+        AbstractSimpleScreenShotTestBenchTest {
+
+    protected String getTestViewName() {
+        return TimelineWithSpike.class.getName();
+    }
+
+}
