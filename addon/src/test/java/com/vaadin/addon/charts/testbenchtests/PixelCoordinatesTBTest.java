41ae8df2c5de04ca25f7eda4c9e68d0f4d7eb029
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-05-15 07:41:43 +0000

    test script from #11847
    git-svn-id: http://dev.vaadin.com/svn/addons/vaadin-charts@25948 de684a11-831f-0410-ab27-cb86445eb682

---
+++ addon/src/test/java/com/vaadin/addon/charts/testbenchtests/PixelCoordinatesTBTest.java
@@ -0,0 +1,41 @@
+package com.vaadin.addon.charts.testbenchtests;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+import com.vaadin.addon.charts.demoandtestapp.lineandscatter.BasicLineGettingMousePointerPosition;
+import com.vaadin.testbench.By;
+
+public class PixelCoordinatesTBTest extends
+        AbstractSimpleScreenShotTestBenchTest {
+
+    protected String getTestViewName() {
+        String simpleName = BasicLineGettingMousePointerPosition.class
+                .getName();
+        return simpleName;
+    }
+
+    @Override
+    protected void testCustomStuff() {
+        WebElement findElement = driver.findElement(By.id("chart"));
+        Action click;
+        // Point click, needs to hover on point before click
+        // Note, coordinates are for the point, not for the click, so exactly
+        // 80,315 should not be expected in UI
+        click = new Actions(driver).moveToElement(findElement, 80, 315).build();
+        click.perform();
+        sleep(100);
+        click = new Actions(driver).click().build();
+        click.perform();
+
+        // Chart click
+        click = new Actions(driver).moveToElement(findElement, 100, 100)
+                .click().build();
+
+        click.perform();
+
+        sleep(500);
+
+    }
+}
