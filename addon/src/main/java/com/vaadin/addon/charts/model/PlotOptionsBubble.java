cb744b3dc006da8c58ccba2ca19d20475515836f
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-05-27 12:55:28 +0000

    fixes #11927: min/max size support for bubbles
    git-svn-id: http://dev.vaadin.com/svn/addons/vaadin-charts@25996 de684a11-831f-0410-ab27-cb86445eb682

--- addon/src/main/java/com/vaadin/addon/charts/model/PlotOptionsBubble.java
+++ addon/src/main/java/com/vaadin/addon/charts/model/PlotOptionsBubble.java
@@ -1,8 +1,12 @@
 package com.vaadin.addon.charts.model;
 
+import java.io.Serializable;
+
 public class PlotOptionsBubble extends AbstractLinePlotOptions {
 
     private Boolean displayNegative;
+    private Serializable minSize;
+    private Serializable maxSize;
 
     @Override
     public ChartType getChartType() {
@@ -27,6 +31,76 @@ public class PlotOptionsBubble extends AbstractLinePlotOptions {
      */
     public void setDisplayNegative(Boolean displayNegative) {
         this.displayNegative = displayNegative;
+    }
+
+    /**
+     * @return the minSize as a number of pixels or percentage string (e.g.
+     *         "10%")
+     * @see #setMinSize(Number)
+     */
+    public Serializable getMinSize() {
+        return minSize;
+    }
+
+    /**
+     * Sets the minimum bubble size in pixels. Bubbles will automatically size
+     * between the minSize and maxSize to reflect the z value of each bubble.
+     * Defaults to 20%.
+     * 
+     * @param minSize
+     *            the minSize to set
+     * 
+     * @see #setMinSizeAsPercentage(Number)
+     */
+    public void setMinSize(Number minSize) {
+        this.minSize = minSize;
+    }
+
+    /**
+     * Sets the minimum bubble size in a percentage of the smallest one of the
+     * plot width and height. Bubbles will automatically size between the
+     * minSize and maxSize to reflect the z value of each bubble. Defaults to 20
+     * (%).
+     * 
+     * @param minSize
+     * @see #setMinSize(Number)
+     */
+    public void setMinSizeAsPercentage(Number minSize) {
+        this.minSize = minSize + "%";
+    }
+
+    /**
+     * @return the maxSize as a number of pixels or percentage string (e.g.
+     *         "10%")
+     */
+    public Serializable getMaxSize() {
+        return maxSize;
+    }
+
+    /**
+     * Sets the maximum bubble size in pixels. Bubbles will automatically size
+     * between the minSize and maxSize to reflect the z value of each bubble.
+     * Defaults to 20%.
+     * 
+     * @param maxSize
+     *            the maxSize to set
+     * @see #setMaxSizeAsPercentage(Number)
+     */
+    public void setMaxSize(Number maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    /**
+     * Sets the maximum bubble size in a percentage of the smallest one of the
+     * plot width and height. Bubbles will automatically size between the
+     * minSize and maxSize to reflect the z value of each bubble. Defaults to 20
+     * (%).
+     * 
+     * @param maxSize
+     * @see #setMaxSize(Number)
+     */
+    public void setMaxSizeAsPercentage(Number maxSize) {
+        this.maxSize = maxSize;
     }
 
 }
