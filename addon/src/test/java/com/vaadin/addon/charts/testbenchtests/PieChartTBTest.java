c16937945c256015fa82f2e469a1dc86eed6ccb3
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-06-18 08:43:39 +0000

    regression test for  #12093
    git-svn-id: http://dev.vaadin.com/svn/addons/vaadin-charts@26072 de684a11-831f-0410-ab27-cb86445eb682

--- addon/src/test/java/com/vaadin/addon/charts/testbenchtests/PieChartTBTest.java
+++ addon/src/test/java/com/vaadin/addon/charts/testbenchtests/PieChartTBTest.java
@@ -1,5 +1,12 @@
 package com.vaadin.addon.charts.testbenchtests;
 
+import junit.framework.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
 import com.vaadin.addon.charts.demoandtestapp.pie.PieChart;
 
 public class PieChartTBTest extends AbstractSimpleScreenShotTestBenchTest {
@@ -12,5 +19,22 @@ public class PieChartTBTest extends AbstractSimpleScreenShotTestBenchTest {
     @Override
     protected String getPackageName() {
         return "pie";
+    }
+
+    @Override
+    protected void testCustomStuff() {
+        super.testCustomStuff();
+        WebElement chart = driver.findElement(By
+                .xpath("//div[contains(@class, 'vaadin-chart')]"));
+        Action click = new Actions(driver).moveToElement(chart, 400 + 20, 200)
+                .click().build();
+
+        click.perform();
+
+        boolean containsFirefox = driver
+                .findElement(By.className("v-Notification")).getText()
+                .contains("Firefox");
+        Assert.assertTrue(containsFirefox);
+
     }
 }
