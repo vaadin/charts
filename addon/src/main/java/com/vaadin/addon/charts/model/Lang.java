488c94830f5a18d6f19a20e239279ab9fa1792bd
Miki <miki@vaadin.com>
2014-11-20 13:49:53 +0200

    language options up to date with the latest highcharts #15159
    Change-Id: I07004924ee51a6d1ca251515bb1e59a04bfeca43

--- addon/src/main/java/com/vaadin/addon/charts/model/Lang.java
+++ addon/src/main/java/com/vaadin/addon/charts/model/Lang.java
@@ -22,15 +22,26 @@ package com.vaadin.addon.charts.model;
  * cannot be set on each chart initiation.
  */
 public class Lang extends AbstractConfigurationObject {
-    private String loading;
+
+    private static final long serialVersionUID = 20141120;
+
+    private String contextButtonTitle;
     private String decimalPoint;
-    private String thousandsSep;
+    private String downloadJPEG;
+    private String downloadPDF;
+    private String downloadPNG;
+    private String downloadSVG;
+    private String drillUpText;
+    private String loading;
     private String[] months;
-    private String[] weekdays;
-    private String[] shortMonths;
+    private String noData;
     private String[] numericSymbols;
+    private String printChart;
     private String resetZoom;
     private String resetZoomTitle;
+    private String[] shortMonths;
+    private String thousandsSep;
+    private String[] weekdays;
 
     /**
      * @see #setLoading(String)
@@ -45,12 +56,15 @@ public class Lang extends AbstractConfigurationObject {
      * Sets the loading text that is shown while loading charts.
      * 
      * @param loading
+     *            New loading text.
      */
     public void setLoading(String loading) {
         this.loading = loading;
     }
 
     /**
+     * Returns the decimal point.
+     * 
      * @see #setDecimalPoint(String)
      * @return The decimal point or null if not defined
      */
@@ -62,13 +76,16 @@ public class Lang extends AbstractConfigurationObject {
      * Sets the decimal point to use
      * 
      * @param decimalPoint
+     *            Decimal point symbol.
      */
     public void setDecimalPoint(String decimalPoint) {
         this.decimalPoint = decimalPoint;
     }
 
     /**
-     * @see #setThousandsSep()
+     * Returns the thousands separator.
+     * 
+     * @see #setThousandsSep(String)
      * @return The thousands separator point or null if not defined
      */
     public String getThousandsSep() {
@@ -79,6 +96,7 @@ public class Lang extends AbstractConfigurationObject {
      * Sets the thousands separator to use
      * 
      * @param thousandsSep
+     *            New thousand separator.
      */
     public void setThousandsSep(String thousandsSep) {
         this.thousandsSep = thousandsSep;
@@ -98,8 +116,10 @@ public class Lang extends AbstractConfigurationObject {
     }
 
     /**
+     * Returns current month names.
+     * 
      * @see #setMonths(String[])
-     * @return
+     * @return Current month names.
      */
     public String[] getMonths() {
         return months;
@@ -118,8 +138,10 @@ public class Lang extends AbstractConfigurationObject {
     }
 
     /**
+     * Returns current weekday names.
+     * 
      * @see #setWeekdays(String[])
-     * @return
+     * @return Weekday names.
      */
     public String[] getWeekdays() {
         return weekdays;
@@ -138,8 +160,10 @@ public class Lang extends AbstractConfigurationObject {
     }
 
     /**
+     * Returns the array with short month names.
+     * 
      * @see #setShortMonths(String[])
-     * @return
+     * @return Array with short month names.
      */
     public String[] getShortMonths() {
         return shortMonths;
@@ -157,8 +181,10 @@ public class Lang extends AbstractConfigurationObject {
     }
 
     /**
+     * Returns numeric symbols.
+     * 
      * @see #setNumericSymbols(String[])
-     * @return
+     * @return An array with numeric symbols.
      */
     public String[] getNumericSymbols() {
         return numericSymbols;
@@ -168,14 +194,17 @@ public class Lang extends AbstractConfigurationObject {
      * Sets the title of the zoom resetting button. Defaults to "Reset zoom".
      * 
      * @param resetZoom
+     *            Tile for the zoom-resetting button.
      */
     public void setResetZoom(String resetZoom) {
         this.resetZoom = resetZoom;
     }
 
     /**
+     * Returns current title for the zoom-resetting button.
+     * 
      * @see #setResetZoom(String)
-     * @return
+     * @return Title for zoom-resetting button.
      */
     public String getResetZoom() {
         return resetZoom;
@@ -185,16 +214,173 @@ public class Lang extends AbstractConfigurationObject {
      * Title of the zoom resetting button. Defaults to "Reset zoom level 1:1".
      * 
      * @param resetZoomTitle
+     *            New title.
      */
     public void setResetZoomTitle(String resetZoomTitle) {
         this.resetZoomTitle = resetZoomTitle;
     }
 
     /**
+     * Returns the title for zoom reset button.
+     * 
      * @see #setResetZoomTitle(String)
-     * @return
+     * @return The title for zoom reset button.
      */
     public String getResetZoomTitle() {
         return resetZoomTitle;
+    }
+
+    /**
+     * Returns the tooltip title for the context menu.
+     * 
+     * @return The tooltip title for the context menu.
+     */
+    public String getContextButtonTitle() {
+        return contextButtonTitle;
+    }
+
+    /**
+     * Sets the tooltip title for the context menu.
+     * 
+     * @param contextButtonTitle
+     *            New tooltip.
+     */
+    public void setContextButtonTitle(String contextButtonTitle) {
+        this.contextButtonTitle = contextButtonTitle;
+    }
+
+    /**
+     * The text for the JPEG download menu item.
+     * 
+     * @return The text for the JPEG download menu item.
+     */
+    public String getDownloadJPEG() {
+        return downloadJPEG;
+    }
+
+    /**
+     * Sets the text for download JPEG.
+     * 
+     * @param downloadJPEG
+     *            New text.
+     */
+    public void setDownloadJPEG(String downloadJPEG) {
+        this.downloadJPEG = downloadJPEG;
+    }
+
+    /**
+     * The text for the PDF download menu item.
+     * 
+     * @return The text for the PDF download menu item.
+     */
+    public String getDownloadPDF() {
+        return downloadPDF;
+    }
+
+    /**
+     * Sets the text for download PDF.
+     * 
+     * @param downloadPDF
+     *            New text.
+     */
+    public void setDownloadPDF(String downloadPDF) {
+        this.downloadPDF = downloadPDF;
+    }
+
+    /**
+     * Returns the text for downloading PNG.
+     * 
+     * @return Text for downloading PNG.
+     */
+    public String getDownloadPNG() {
+        return downloadPNG;
+    }
+
+    /**
+     * Sets the text for downloading PNG.
+     * 
+     * @param downloadPNG
+     *            New text.
+     */
+    public void setDownloadPNG(String downloadPNG) {
+        this.downloadPNG = downloadPNG;
+    }
+
+    /**
+     * Returns the text for downloading SVG.
+     * 
+     * @return Text for downloading SVG.
+     */
+    public String getDownloadSVG() {
+        return downloadSVG;
+    }
+
+    /**
+     * Sets new text for downloading SVG.
+     * 
+     * @param downloadSVG
+     *            New text.
+     */
+    public void setDownloadSVG(String downloadSVG) {
+        this.downloadSVG = downloadSVG;
+    }
+
+    /**
+     * Gets the text for the button that appears when drilling down.
+     * 
+     * @return Text for the button for drilling up.
+     */
+    public String getDrillUpText() {
+        return drillUpText;
+    }
+
+    /**
+     * Sets the text for the button that appears when drilling down, linking
+     * back to the parent series. The parent series' name is inserted for
+     * <tt>{series.name}</tt>. Defaults to <tt>Back to {series.name}</tt>.
+     * 
+     * @param drillUpText
+     *            New text.
+     */
+    public void setDrillUpText(String drillUpText) {
+        this.drillUpText = drillUpText;
+    }
+
+    /**
+     * Returns text to show when there is no data.
+     * 
+     * @return Text to show when there is no data.
+     */
+    public String getNoData() {
+        return noData;
+    }
+
+    /**
+     * Sets the text to show when there is no data.
+     * 
+     * @param noData
+     *            New text.
+     */
+    public void setNoData(String noData) {
+        this.noData = noData;
+    }
+
+    /**
+     * Returns the menu item text for printing the chart.
+     * 
+     * @return The text for printing the chart.
+     */
+    public String getPrintChart() {
+        return printChart;
+    }
+
+    /**
+     * Sets the text of a menu item to print the chart.
+     * 
+     * @param printChart
+     *            New text.
+     */
+    public void setPrintChart(String printChart) {
+        this.printChart = printChart;
     }
 }
