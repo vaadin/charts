cb8402761aac49588ea8ab2001c2fe13d0ac43c8
Guillermo Alvarez <guillermo@vaadin.com>
2015-04-29 15:04:01 +0300

    Fix sources declaration for charts widgetset
    Change-Id: I1ecbeda23569a79165cec7812b7f4d27e5efa6c9

--- addon/src/test/java/com/vaadin/addon/charts/testbenchtests/ColumnWithShapedTooltipTBTest.java
+++ addon/src/test/java/com/vaadin/addon/charts/testbenchtests/ColumnWithShapedTooltipTBTest.java
@@ -38,6 +38,7 @@ public class ColumnWithShapedTooltipTBTest extends
     public List<DesiredCapabilities> getBrowsersToTest() {
         List<DesiredCapabilities> result = super.getBrowsersToTest();
         result.remove(Browser.IE8.getDesiredCapabilities());
+        result.remove(Browser.PHANTOMJS.getDesiredCapabilities());
         result.remove(Browser.FIREFOX.getDesiredCapabilities());
         return result;
     }
