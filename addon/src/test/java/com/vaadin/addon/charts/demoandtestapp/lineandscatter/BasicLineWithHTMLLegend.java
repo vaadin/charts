77025da9848a3ffcd560fcfd871784645882ee0a
Jonatan Kronqvist <jonatan@vaadin.com>
2015-05-20 12:42:46 +0300

    Allow use of HTML in Legend items to ease styling #17919
    Change-Id: I10b7806a47e0c840916e9d9757429687dfbbf41f
    (cherry picked from commit 457d8f9f0e1fc3aca9d6fff0a6c2e3928ce5c6c6)

---
+++ addon/src/test/java/com/vaadin/addon/charts/demoandtestapp/lineandscatter/BasicLineWithHTMLLegend.java
@@ -0,0 +1,92 @@
+package com.vaadin.addon.charts.demoandtestapp.lineandscatter;
+
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.demoandtestapp.AbstractVaadinChartExample;
+import com.vaadin.addon.charts.demoandtestapp.SkipFromDemo;
+import com.vaadin.addon.charts.model.Axis;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.HorizontalAlign;
+import com.vaadin.addon.charts.model.Labels;
+import com.vaadin.addon.charts.model.LayoutDirection;
+import com.vaadin.addon.charts.model.Legend;
+import com.vaadin.addon.charts.model.ListSeries;
+import com.vaadin.addon.charts.model.PlotOptionsLine;
+import com.vaadin.addon.charts.model.Title;
+import com.vaadin.addon.charts.model.VerticalAlign;
+import com.vaadin.ui.Component;
+
+@SkipFromDemo
+public class BasicLineWithHTMLLegend extends AbstractVaadinChartExample {
+
+    @Override
+    public String getDescription() {
+        return "Basic Line With Data Labels";
+    }
+
+    @Override
+    protected Component getChart() {
+        Chart chart = new Chart();
+        chart.setHeight("450px");
+        chart.setWidth("100%");
+
+        Configuration configuration = chart.getConfiguration();
+        configuration.getChart().setType(ChartType.LINE);
+        configuration.getChart().setMarginRight(130);
+        configuration.getChart().setMarginBottom(25);
+
+        configuration.getTitle().setText("Monthly Average Temperature");
+        configuration.getSubTitle().setText("Source: WorldClimate.com");
+
+        configuration.getxAxis().setCategories("Jan", "Feb", "Mar", "Apr",
+                "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+        Axis yAxis = configuration.getyAxis();
+        yAxis.setMin(-5d);
+        yAxis.setTitle(new Title("Temperature (°C)"));
+        yAxis.getTitle().setVerticalAlign(VerticalAlign.HIGH);
+
+        configuration
+                .getTooltip()
+                .setFormatter(
+                        "'<b>'+ this.series.name +'</b><br/>'+this.x +': '+ this.y +'°C'");
+
+        PlotOptionsLine plotOptions = new PlotOptionsLine();
+        plotOptions.setDataLabels(new Labels(true));
+        configuration.setPlotOptions(plotOptions);
+
+        Legend legend = configuration.getLegend();
+        legend.setUseHTML(true);
+        legend.setLayout(LayoutDirection.VERTICAL);
+        legend.setHorizontalAlign(HorizontalAlign.RIGHT);
+        legend.setVerticalAlign(VerticalAlign.TOP);
+        legend.setX(-10d);
+        legend.setY(100d);
+        legend.setBorderWidth(0);
+
+        ListSeries ls = new ListSeries();
+        ls.setName("<i>Tokyo</i>");
+        ls.setData(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3,
+                13.9, 9.6);
+        configuration.addSeries(ls);
+        ls = new ListSeries();
+        ls.setName("<span style='font-family:serif;'>New York</span>");
+        ls.setData(-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1,
+                8.6, 2.5);
+        configuration.addSeries(ls);
+        ls = new ListSeries();
+        ls.setName("Berlin");
+        ls.setData(-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9,
+                1.0);
+        configuration.addSeries(ls);
+        ls = new ListSeries();
+        ls.setName("London");
+        ls.setData(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6,
+                4.8);
+        configuration.addSeries(ls);
+
+        chart.drawChart(configuration);
+        return chart;
+    }
+
+}
