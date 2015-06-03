01ac1311d0932f3f7c10ad9f90fa5f5d6425345a
Guillermo Alvarez <guillermo@vaadin.com>
2015-06-03 16:24:22 +0300

    Add see also to plotoptions implementations for each ChartType (CHARTS-99)
    Change-Id: I129c11cd778a182291e3eb939fd2c05ca3bbaf39

--- addon/src/main/java/com/vaadin/addon/charts/model/ChartType.java
+++ addon/src/main/java/com/vaadin/addon/charts/model/ChartType.java
@@ -23,31 +23,101 @@ package com.vaadin.addon.charts.model;
  * COLUMNRANGE. Defaults to LINE.
  * <p>
  * Own custom extensions can be made by extending this class.
+ * <p>
+ * Configuration specific to each ChartType can be set using
+ * {@link Configuration#setPlotOptions(AbstractPlotOptions)} or for a single
+ * series using {@link AbstractSeries#setPlotOptions(AbstractPlotOptions)}
  */
 public class ChartType implements ChartEnum {
 
+    /**
+     * @see PlotOptionsArea
+     */
     public static final ChartType AREA = new ChartType("area");
+    /**
+     * @see PlotOptionsLine
+     */
     public static final ChartType LINE = new ChartType("line");
+    /**
+     * @see PlotOptionsSpline
+     */
     public static final ChartType SPLINE = new ChartType("spline");
+    /**
+     * @see PlotOptionsAreaSpline
+     */
     public static final ChartType AREASPLINE = new ChartType("areaspline");
+    /**
+     * @see PlotOptionsColumn
+     */
     public static final ChartType COLUMN = new ChartType("column");
+    /**
+     * @see PlotOptionsBar
+     */
     public static final ChartType BAR = new ChartType("bar");
+    /**
+     * @see PlotOptionsPie
+     */
     public static final ChartType PIE = new ChartType("pie");
+    /**
+     * @see PlotOptionsScatter
+     */
     public static final ChartType SCATTER = new ChartType("scatter");
+    /**
+     * @see PlotOptionsGauge
+     */
     public static final ChartType GAUGE = new ChartType("gauge");
+    /**
+     * @see PlotOptionsAreaRange
+     */
     public static final ChartType AREARANGE = new ChartType("arearange");
+    /**
+     * @see PlotOptionsColumnRange
+     */
     public static final ChartType COLUMNRANGE = new ChartType("columnrange");
+    /**
+     * @see PlotOptionsAreaSplineRange
+     */
     public static final ChartType AREASPLINERANGE = new ChartType(
             "areasplinerange");
+    /**
+     * @see PlotOptionsBoxPlot
+     */
     public static final ChartType BOXPLOT = new ChartType("boxplot");
+    /**
+     * @see PlotOptionsErrorBar
+     */
     public static final ChartType ERRORBAR = new ChartType("errorbar");
+    /**
+     * @see PlotOptionsBubble
+     */
     public static final ChartType BUBBLE = new ChartType("bubble");
+    /**
+     * @see PlotOptionsFunnel
+     */
     public static final ChartType FUNNEL = new ChartType("funnel");
+    /**
+     * @see PlotOptionsWaterfall
+     */
     public static final ChartType WATERFALL = new ChartType("waterfall");
+    /**
+     * @see PlotOptionsPyramid
+     */
     public static final ChartType PYRAMID = new ChartType("pyramid");
+    /**
+     * @see PlotOptionsSolidGauge
+     */
     public static final ChartType SOLIDGAUGE = new ChartType("solidgauge");
+    /**
+     * @see PlotOptionsHeatMap
+     */
     public static final ChartType HEATMAP = new ChartType("heatmap");
+    /**
+     * @see PlotOptionsTreeMap
+     */
     public static final ChartType TREEMAP = new ChartType("treemap");
+    /**
+     * @see PlotOptionsPolygon
+     */
     public static final ChartType POLYGON = new ChartType("polygon");
 
     private final String type;
