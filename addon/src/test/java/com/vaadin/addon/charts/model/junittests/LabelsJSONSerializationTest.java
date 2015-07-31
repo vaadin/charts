d35a46060306d7381ff190dee13ea7967f6aa1f5
Guillermo Alvarez <guillermo@vaadin.com>
2015-07-31 21:19:55 +0300

    Add missing allowOverlap and padding API to Labels (#18513)
    Change-Id: I2abdbc34b8caf01621b960c6699ded90c4a29457

--- addon/src/test/java/com/vaadin/addon/charts/model/junittests/LabelsJSONSerializationTest.java
+++ addon/src/test/java/com/vaadin/addon/charts/model/junittests/LabelsJSONSerializationTest.java
@@ -14,13 +14,48 @@ import com.vaadin.addon.charts.model.Labels;
 public class LabelsJSONSerializationTest {
 
     @Test
-    public void toString_autoRotationLimitIsSet_LabelsSerializedWithAutoRotationLimit() {
+    public void toString_enabledSet_labelsSerializedWithEnabled() {
+
+        Labels labels = new Labels(false);
+
+        String json = labels.toString();
+        String expected = "{\"enabled\":false}";
+
+        assertEquals(expected, json);
+    }
+
+    @Test
+    public void toString_autoRotationLimitIsSet_labelsSerializedWithAutoRotationLimit() {
 
         Labels labels = new Labels(true);
         labels.setAutoRotationLimit(40);
 
         String json = labels.toString();
         String expected = "{\"enabled\":true,\"autoRotationLimit\":40}";
+
+        assertEquals(expected, json);
+    }
+
+    @Test
+    public void toString_paddingIsSet_labelsSerializedWithPadding() {
+
+        Labels labels = new Labels(true);
+        labels.setPadding(8);
+
+        String json = labels.toString();
+        String expected = "{\"enabled\":true,\"padding\":8}";
+
+        assertEquals(expected, json);
+    }
+
+    @Test
+    public void toString_allowOverlapIsSet_labelsSerializedWithAllowOverlap() {
+
+        Labels labels = new Labels(true);
+        labels.setAllowOverlap(true);
+
+        String json = labels.toString();
+        String expected = "{\"enabled\":true,\"allowOverlap\":true}";
 
         assertEquals(expected, json);
     }
