308f3ef8a37ec3876e70a0a597247c18edd62926
Guillermo Alvarez <guillermo@vaadin.com>
2015-12-21 16:54:24 -0300

    Remove duplicate license header from ChartServerRpc
    Change-Id: I7285db51acfef7263aef728e763ad8731848693e

--- addon/src/main/java/com/vaadin/addon/charts/shared/ChartServerRpc.java
+++ addon/src/main/java/com/vaadin/addon/charts/shared/ChartServerRpc.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 Vaadin Community.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.vaadin.addon.charts.shared;
 
 /*
@@ -50,8 +35,7 @@ public interface ChartServerRpc extends ServerRpc {
 
     void onLegendItemClick(int seriesIndex, int seriesItemIndex);
 
-    void onCheckboxClick(boolean isChecked, int seriesIndex,
-        int seriesItemIndex);
+    void onCheckboxClick(boolean isChecked, int seriesIndex, int seriesItemIndex);
 
     void onSeriesHide(int seriesIndex, int seriesItemIndex);
 
