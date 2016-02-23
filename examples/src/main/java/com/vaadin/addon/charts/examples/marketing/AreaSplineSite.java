198106ea4cdefee0e3299304bfe57561dfc885dd
Guillermo Alvarez <guillermo@vaadin.com>
2016-02-23 09:59:03 +0200

    Add sources used in charts site (CHARTS-389)
    These demos have no particular test associated
    Changes done in these classes should be notified to marketing
    
    Change-Id: If1f540053d851190ddd5a77009ba848eaef104aa

---
+++ examples/src/main/java/com/vaadin/addon/charts/examples/marketing/AreaSplineSite.java
@@ -0,0 +1,61 @@
+package com.vaadin.addon.charts.examples.marketing;
+
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.examples.AbstractVaadinChartExample;
+import com.vaadin.addon.charts.examples.SkipFromDemo;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.HorizontalAlign;
+import com.vaadin.addon.charts.model.LayoutDirection;
+import com.vaadin.addon.charts.model.Legend;
+import com.vaadin.addon.charts.model.ListSeries;
+import com.vaadin.addon.charts.model.Tooltip;
+import com.vaadin.addon.charts.model.VerticalAlign;
+import com.vaadin.addon.charts.model.XAxis;
+import com.vaadin.addon.charts.model.YAxis;
+import com.vaadin.ui.Component;
+
+@SuppressWarnings("serial")
+@SkipFromDemo
+public class AreaSplineSite extends AbstractVaadinChartExample {
+
+    @Override
+    public String getDescription() {
+        return "Area-Spline demo used in charts site";
+    }
+
+    @Override
+    protected Component getChart() {
+        Chart chart = new Chart(ChartType.AREASPLINE);
+
+        Configuration conf = chart.getConfiguration();
+
+        conf.setTitle("Monthly supply and demand");
+        conf.setSubTitle("Last six months");
+
+        Legend legend = conf.getLegend();
+        legend.setLayout(LayoutDirection.VERTICAL);
+        legend.setAlign(HorizontalAlign.LEFT);
+        legend.setFloating(true);
+        legend.setVerticalAlign(VerticalAlign.TOP);
+        legend.setX(150);
+        legend.setY(100);
+
+        Tooltip tooltip = conf.getTooltip();
+        tooltip.setValueSuffix("t");
+        tooltip.setShared(true);
+
+        XAxis xAxis = conf.getxAxis();
+        xAxis.setCategories("January", "February", "March", "April", "May",
+                "June");
+
+        YAxis yAxis = conf.getyAxis();
+        yAxis.setTitle("Metric tons");
+
+        conf.addSeries(new ListSeries("Supply", 35, 28, 45, 60, 80, 74));
+        conf.addSeries(new ListSeries("Demand", 29, 11, 50, 63, 65, 61));
+
+        return chart;
+    }
+
+}
---
+++ examples/src/main/java/com/vaadin/addon/charts/examples/marketing/ColumnSite.java
@@ -0,0 +1,48 @@
+package com.vaadin.addon.charts.examples.marketing;
+
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.examples.AbstractVaadinChartExample;
+import com.vaadin.addon.charts.examples.SkipFromDemo;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.ListSeries;
+import com.vaadin.addon.charts.model.Tooltip;
+import com.vaadin.addon.charts.model.XAxis;
+import com.vaadin.addon.charts.model.YAxis;
+import com.vaadin.ui.Component;
+
+@SuppressWarnings("serial")
+@SkipFromDemo
+public class ColumnSite extends AbstractVaadinChartExample {
+
+    @Override
+    public String getDescription() {
+        return "Column demo used in charts site";
+    }
+
+    @Override
+    protected Component getChart() {
+        Chart chart = new Chart(ChartType.COLUMN);
+
+        Configuration conf = chart.getConfiguration();
+
+        conf.setTitle("Earnings");
+        conf.setSubTitle("2011 - 2014");
+
+        XAxis xAxis = conf.getxAxis();
+        xAxis.setCategories("2011", "2012", "2013", "2014");
+
+        YAxis yAxis = conf.getyAxis();
+        yAxis.setTitle("B€");
+
+        Tooltip tooltip = conf.getTooltip();
+        tooltip.setPointFormat("{series.name}: {point.y} B€");
+
+        conf.addSeries(new ListSeries("Revenue", 1.5, 1.8, 1.2, 2.3));
+        conf.addSeries(new ListSeries("Expenses", 1.2, 1.1, 1.3, 0.9));
+        conf.addSeries(new ListSeries("Net income", -0.3, 0.7, -0.1, 1.4));
+
+        return chart;
+    }
+
+}
