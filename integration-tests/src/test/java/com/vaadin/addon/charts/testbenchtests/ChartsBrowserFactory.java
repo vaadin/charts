81066c914e74d8e414a305d77a6c7591c4dd499b
jtomass <juhani@vaadin.com>
2016-11-23 15:36:50 +0200

    Updated to work with saucelabs
    Copied from vaadin fw

--- integration-tests/src/test/java/com/vaadin/addon/charts/testbenchtests/ChartsBrowserFactory.java
+++ integration-tests/src/test/java/com/vaadin/addon/charts/testbenchtests/ChartsBrowserFactory.java
@@ -13,43 +13,73 @@ public class ChartsBrowserFactory extends DefaultBrowserFactory {
 
     @Override
     public DesiredCapabilities create(Browser browser) {
-        switch (browser) {
-        case IE11:
-            return createIE(browser, "11");
-        case PHANTOMJS:
-            DesiredCapabilities phantom2 = create(browser, "2", Platform.LINUX);
-            // Hack for the test cluster
-            phantom2.setCapability("phantomjs.binary.path", "/usr/bin/phantomjs2");
-            return phantom2;
-        case CHROME:
-            return create(browser, "40", Platform.VISTA);
-        case FIREFOX:
-        default:
-            return createFirefox(browser);
+        String version = "";
+        // need to pick a version to request, but for these two auto-updating
+        // browsers there is a special value "latest" (and "latest-1",
+        // "latest-2")
+        if (Browser.FIREFOX.equals(browser) || Browser.CHROME.equals(browser)) {
+            version = "latest";
         }
-    }
-
-    private DesiredCapabilities createFirefox(Browser browser) {
-        DesiredCapabilities desiredCapabilities =
-            create(browser, "24", Platform.XP);
-        // Configuring an empty Firefox profile fixes tests
-        // where a data point is clicked.
-        //    - pointClick, select and unselect in ServerSideEvents tests
-        FirefoxProfile firefoxProfile = new FirefoxProfile();
-        desiredCapabilities.setCapability(FirefoxDriver.PROFILE, firefoxProfile);
-        return desiredCapabilities;
-    }
-
-    private DesiredCapabilities createIE(Browser browser, String version) {
-        DesiredCapabilities capabilities = create(browser, version,
-                Platform.WINDOWS);
-        capabilities.setCapability(
-                InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
-        return capabilities;
+        return create(browser, version, Platform.ANY);
     }
 
     @Override
-    public DesiredCapabilities create(Browser browser, String version) {
-        return create(browser);
+    public DesiredCapabilities create(Browser browser, String version,
+            Platform platform) {
+        DesiredCapabilities caps;
+
+        switch (browser) {
+        case CHROME:
+            caps = DesiredCapabilities.chrome();
+            caps.setVersion(version);
+            break;
+        case PHANTOMJS:
+            // This will not work on SauceLabs - should be filtered with
+            // browsers.exclude. However, we cannot throw an exception here as
+            // filtering only takes place if there is no exception.
+            caps = DesiredCapabilities.phantomjs();
+            caps.setVersion("1");
+            caps.setPlatform(Platform.LINUX);
+            break;
+        case SAFARI:
+            caps = DesiredCapabilities.safari();
+            caps.setVersion(version);
+            break;
+        case IE11:
+            caps = DesiredCapabilities.internetExplorer();
+            caps.setVersion("11.0");
+            // There are 2 capabilities ie.ensureCleanSession and
+            // ensureCleanSession in Selenium
+            // IE 11 uses ie.ensureCleanSession
+            caps.setCapability("ie.ensureCleanSession", true);
+            caps.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION,
+                    true);
+            break;
+        case FIREFOX:
+            caps = DesiredCapabilities.firefox();
+            caps.setVersion(version);
+            break;
+        default:
+            caps = DesiredCapabilities.firefox();
+            caps.setVersion(version);
+            caps.setPlatform(platform);
+        }
+
+        if (!Browser.PHANTOMJS.equals(browser)) {
+            caps.setCapability("platform", "Windows 7");
+        }
+
+        // accept self-signed certificates
+        caps.setCapability("acceptSslCerts", "true");
+
+        // SauceLabs specific parts
+
+        caps.setCapability("screenResolution", "1680x1050");
+
+        // build and project for easy identification in SauceLabs UI
+        caps.setCapability("build", Version.getFullVersion());
+
+        return caps;
     }
+
 }
