0711a553aede0e29977bc07e6c9154c4ee0588d2
Guillermo Alvarez <guillermo@vaadin.com>
2016-11-29 16:49:01 +0200

    Undo ohlc changes

--- examples/src/main/java/com/vaadin/addon/charts/examples/timeline/Ohlc.java
+++ examples/src/main/java/com/vaadin/addon/charts/examples/timeline/Ohlc.java
@@ -1,21 +1,17 @@
 package com.vaadin.addon.charts.examples.timeline;
 
-import java.util.List;
-
 import com.vaadin.addon.charts.Chart;
 import com.vaadin.addon.charts.examples.AbstractVaadinChartExample;
 import com.vaadin.addon.charts.examples.timeline.util.StockPrices;
-import com.vaadin.addon.charts.examples.timeline.util.StockPrices.OhlcData;
 import com.vaadin.addon.charts.model.ChartType;
 import com.vaadin.addon.charts.model.Configuration;
 import com.vaadin.addon.charts.model.DataGrouping;
-import com.vaadin.addon.charts.model.DataProviderSeries;
+import com.vaadin.addon.charts.model.DataSeries;
+import com.vaadin.addon.charts.model.OhlcItem;
 import com.vaadin.addon.charts.model.PlotOptionsOhlc;
 import com.vaadin.addon.charts.model.RangeSelector;
 import com.vaadin.addon.charts.model.TimeUnit;
 import com.vaadin.addon.charts.model.TimeUnitMultiples;
-import com.vaadin.server.data.DataProvider;
-import com.vaadin.server.data.ListDataProvider;
 import com.vaadin.ui.Component;
 
 public class Ohlc extends AbstractVaadinChartExample {
@@ -35,27 +31,29 @@ public class Ohlc extends AbstractVaadinChartExample {
         Configuration configuration = chart.getConfiguration();
         configuration.getTitle().setText("AAPL Stock Price");
 
-        List<OhlcData> data = StockPrices.fetchAaplOhlcPrice();
-        DataProvider<OhlcData> ds = new ListDataProvider<>(data);
-        DataProviderSeries<OhlcData> dataSeries = new DataProviderSeries<>(ds);
-        dataSeries.setX(OhlcData::getDate);
-        dataSeries.setLow(OhlcData::getLow);
-        dataSeries.setHigh(OhlcData::getHigh);
-        dataSeries.setClose(OhlcData::getClose);
-        dataSeries.setOpen(OhlcData::getOpen);
-
+        DataSeries dataSeries = new DataSeries();
         PlotOptionsOhlc plotOptionsOhlc = new PlotOptionsOhlc();
         DataGrouping grouping = new DataGrouping();
         grouping.addUnit(new TimeUnitMultiples(TimeUnit.WEEK, 1));
         grouping.addUnit(new TimeUnitMultiples(TimeUnit.MONTH, 1, 2, 3, 4, 6));
         plotOptionsOhlc.setDataGrouping(grouping);
-        plotOptionsOhlc.setTurboThreshold(0);
         dataSeries.setPlotOptions(plotOptionsOhlc);
+        for (StockPrices.OhlcData data : StockPrices.fetchAaplOhlcPrice()) {
+            OhlcItem item = new OhlcItem();
+            item.setX(data.getDate());
+            item.setLow(data.getLow());
+            item.setHigh(data.getHigh());
+            item.setClose(data.getClose());
+            item.setOpen(data.getOpen());
+            dataSeries.add(item);
+        }
         configuration.setSeries(dataSeries);
 
         RangeSelector rangeSelector = new RangeSelector();
         rangeSelector.setSelected(1);
         configuration.setRangeSelector(rangeSelector);
+
+        chart.drawChart(configuration);
 
         return chart;
     }
