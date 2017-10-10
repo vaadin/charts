61a450df3cc46870067fea5e894b322683a36f9a
Sayo Oladeji <sayo@vaadin.com>
2017-10-10 17:18:45 +0300

    Basic VaadinChart component for Flow

---
+++ addon/src/main/java/com/vaadin/addon/charts/VaadinChart.java
@@ -0,0 +1,10 @@
+package com.vaadin.addon.charts;
+
+import com.vaadin.annotations.HtmlImport;
+import com.vaadin.annotations.Tag;
+import com.vaadin.ui.Component;
+
+@Tag("vaadin-chart")
+@HtmlImport("frontend://bower_components/vaadin-charts/vaadin-chart.html")
+public class VaadinChart extends Component {
+}
