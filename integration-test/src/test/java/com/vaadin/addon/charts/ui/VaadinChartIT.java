c1897ded56907386153ac74cdc0ab35674fa4a1f
Sayo Oladeji <sayo@vaadin.com>
2017-10-16 17:08:47 +0300

    Updated to TestBench 6.0-SNAPSHOT

--- integration-test/src/test/java/com/vaadin/addon/charts/ui/VaadinChartIT.java
+++ integration-test/src/test/java/com/vaadin/addon/charts/ui/VaadinChartIT.java
@@ -3,30 +3,24 @@ package com.vaadin.addon.charts.ui;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import com.vaadin.testbench.annotations.BrowserConfiguration;
 import com.vaadin.testbench.annotations.RunOnHub;
+import com.vaadin.testbench.parallel.BrowserUtil;
 import com.vaadin.testbench.parallel.ParallelTest;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @RunOnHub
 public class VaadinChartIT extends ParallelTest {
-
-	private static ChromeDriver driver;
-
-	@BeforeClass
-	public static void setupClass() {
-		final ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--headless");
-		driver = new ChromeDriver(chromeOptions);
-	}
 
 	@Override
 	protected String getHubURL() {
@@ -36,32 +30,41 @@ public class VaadinChartIT extends ParallelTest {
 	}
 
 	@Before
-	public void setUp() {
-		driver.get("http://localhost:8080");
-	}
-
-	@After
-	public void tearDown() {
-		driver.quit();
+	public void setUp() throws InterruptedException {
+		getDriver().get("http://localhost:8080");
 	}
 
 	@Test
 	public void checkChartDisplayed() {
 		waitUntilPresent(By.tagName("vaadin-chart"));
-		final WebElement chart = driver.findElementByTagName("vaadin-chart");
+		final WebElement chart = findElement(By.tagName("vaadin-chart"));
 		assertNotNull(chart);
 		final WebElement title = getElementFromShadowRoot(chart, By.className("highcharts-title"));
 		assertTrue(title.getText().contains("First Chart for Flow!"));
 	}
 
 	private void waitUntilPresent(By by) {
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(by));
+		new WebDriverWait(getDriver(), 10).until(ExpectedConditions.presenceOfElementLocated(by));
 	}
 
 	private WebElement getElementFromShadowRoot(WebElement shadowRootOwner, By by) {
-		WebElement shadowRoot = (WebElement) driver.executeScript("return arguments[0].shadowRoot", shadowRootOwner);
+		WebElement shadowRoot = (WebElement) executeScript("return arguments[0].shadowRoot", shadowRootOwner);
 		assertNotNull("Could not locate shadowRoot in the element", shadowRoot);
 		return shadowRoot.findElements(by).stream().findFirst()
 				.orElseThrow(() -> new AssertionError("Could not find required element in the shadowRoot"));
+	}
+
+	/**
+	 * @return all supported browsers which are actively tested
+	 */
+	public List<DesiredCapabilities> getAllBrowsers() {
+		List<DesiredCapabilities> allBrowsers = new ArrayList<>();
+			allBrowsers.add(BrowserUtil.chrome());
+		return Collections.unmodifiableList(allBrowsers);
+	}
+
+	@BrowserConfiguration
+	public List<DesiredCapabilities> getBrowserConfiguration() {
+		return getAllBrowsers();
 	}
 }
