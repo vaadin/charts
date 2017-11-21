a49e4907062a3317109ea55c267a2d4b07abcd2b
Jens Jansson <Peppe@users.noreply.github.com>
2017-11-21 09:17:49 +0200

    Update Util.java

--- model/src/main/java/com/vaadin/addon/charts/util/Util.java
+++ model/src/main/java/com/vaadin/addon/charts/util/Util.java
@@ -39,7 +39,7 @@ public class Util {
      * @return
      */
     public static long toHighchartsTS(Instant date) {
-        return date.getEpochMilli();
+        return date.toEpochMilli();
     }
 
      /**
