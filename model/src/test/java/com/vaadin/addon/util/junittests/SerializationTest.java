6af291fbe518d846e65e66efc4ba83d43c1e54ef
Sven Ruppert <sven.ruppert@gmail.com>
2018-02-02 13:19:30 +0100

    https://github.com/vaadin/charts/issues/488

--- model/src/test/java/com/vaadin/addon/util/junittests/SerializationTest.java
+++ model/src/test/java/com/vaadin/addon/util/junittests/SerializationTest.java
@@ -1,27 +1,28 @@
 package com.vaadin.addon.util.junittests;
 
-import static org.junit.Assert.assertEquals;
+import com.vaadin.addon.charts.util.Util;
+import org.junit.Test;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-import org.junit.Test;
-
-import com.vaadin.addon.charts.util.Util;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for Instant to Highcharts TS conversion.
- *
  */
 public class SerializationTest {
 
-    @Test
-    public void dateUtils_toAndFromHighchartsTS_equalValueObtained() {
-        Instant instant = LocalDateTime.now().toInstant(ZoneOffset.UTC);
-        long highchartsTS = Util.toHighchartsTS(instant);
-        Instant serverInstant = Util.toServerInstant(highchartsTS);
-        assertEquals(instant, serverInstant);
-    }
+  @Test
+  public void dateUtils_toAndFromHighchartsTS_equalValueObtained() {
+    Instant instant       = LocalDateTime.now().toInstant(ZoneOffset.UTC);
+    long    highchartsTS  = Util.toHighchartsTS(instant);
+    Instant serverInstant = Util.toServerInstant(highchartsTS);
+
+    final String serverString = serverInstant.toString();
+    final String strToCompare = serverString.substring(0, serverString.length() - 1);
+    assertTrue(instant.toString().startsWith(strToCompare));
+  }
 
 }
