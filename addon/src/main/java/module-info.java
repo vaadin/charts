11585335e47fb7f974651a3aabd4d2d31e0c637c
Sayo Oladeji <sayo@vaadin.com>
2018-03-19 14:50:33 +0200

    Build on both JDK 8 and 9, run on both Java 8 and 9.

---
+++ addon/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.vaadin.addon.charts {
+    requires java.annotation;
+    requires java.desktop;
+    requires java.logging;
+    requires gentyref;
+    requires gwt.user;
+    requires jackson.core;
+    requires jackson.databind;
+    requires jackson.annotations;
+    requires jsoup;
+    requires vaadin.client;
+    requires vaadin.server;
+    requires vaadin.shared;
+}
