b30c19cd2c4fdccee944ddf9b5b9794177c52838
Guille <alvarezguille@users.noreply.github.com>
2018-03-20 13:22:58 +0200

    Reset drilldownStack on initial response (#502)
    Alternative solution for #483

---
+++ examples/src/main/java/com/vaadin/addon/charts/examples/columnandbar/ChartWithLazyDrilldownInTabSheet.java
@@ -0,0 +1,36 @@
+package com.vaadin.addon.charts.examples.columnandbar;
+
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.examples.AbstractVaadinChartExample;
+import com.vaadin.addon.charts.examples.SkipFromDemo;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TabSheet;
+
+@SkipFromDemo
+public class ChartWithLazyDrilldownInTabSheet
+        extends AbstractVaadinChartExample {
+
+    /**
+     * 
+     * Test UI for #483.
+     * 
+     * To reproduce issue:
+     * <ul>
+     * <li>Click on IE</li>
+     * <li>Change to second tab</li>
+     * <li>Change to first tab</li>
+     * <li>Click on opera</li>
+     * </ul>
+     */
+    @Override
+    protected Component getChart() {
+        TabSheet tabs = new TabSheet();
+        tabs.setSizeFull();
+        Chart chart = (Chart) new ColumnWithNativeLazyDrilldown().getChart();
+        tabs.addTab(chart, "First tab with chart");
+        Label label = new Label("second tab content");
+        tabs.addTab(label, "Other tab");
+        return tabs;
+    }
+}
