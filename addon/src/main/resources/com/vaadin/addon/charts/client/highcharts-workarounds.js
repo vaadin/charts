328fcde318d2a9b66eff2db15982da9f0754023a
Sayo Oladeji <sayo@vaadin.com>
2018-11-08 13:11:11 +0200

    Apply Highcharts DDPoints workaround

--- addon/src/main/resources/com/vaadin/addon/charts/client/highcharts-workarounds.js
+++ addon/src/main/resources/com/vaadin/addon/charts/client/highcharts-workarounds.js
@@ -51,4 +51,23 @@
 
     return result;
   };
+
+  // https://github.com/highcharts/highcharts/issues/5750
+  // http://jsfiddle.net/4q8jyrvm/
+  Highcharts.Axis.prototype.getDDPoints = function(x) {
+    var ret = [];
+    Highcharts.each(this.series, function(series) {
+      var i,
+          xData = series.xData,
+          points = series.points;
+
+      for (i = 0; i < xData.length; i++) {
+        if (xData[i] === x && series.options.data[i] && series.options.data[i].drilldown) {
+          ret.push(points ? points[i] : true);
+          break;
+        }
+      }
+    });
+    return ret;
+  };
 }(Highcharts));
