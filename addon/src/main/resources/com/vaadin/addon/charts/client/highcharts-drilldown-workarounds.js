5bb50e04dd649ca914cd41f06e641cd38315cb34
Guille <alvarezguille@users.noreply.github.com>
2019-04-03 15:55:28 +0300

    Update highcharts version (#574)
    Update to Highcharts 4.2.7
    Apply Highcharts DDPoints workaround
    Remove workaround needed in 4.2.6
    Update screenshot references
    Fixes #525

--- addon/src/main/resources/com/vaadin/addon/charts/client/highcharts-drilldown-workarounds.js
+++ addon/src/main/resources/com/vaadin/addon/charts/client/highcharts-drilldown-workarounds.js
@@ -1,12 +1,21 @@
-  // Workaround for https://github.com/vaadin/charts/issues/460 
-  // from https://github.com/highcharts/highcharts/issues/5565#issuecomment-237712885
 
 (function(Highcharts) {
+  // https://github.com/highcharts/highcharts/issues/5750
+  // http://jsfiddle.net/4q8jyrvm/
+  Highcharts.Axis.prototype.getDDPoints = function(x) {
+    var ret = [];
+    Highcharts.each(this.series, function(series) {
+      var i,
+          xData = series.xData,
+          points = series.points;
 
-  Highcharts.wrap(Highcharts.Chart.prototype, 'drillUp', function (p) {
-    Highcharts.each(this.xAxis, function (axis) {
-        axis.names = [];
+      for (i = 0; i < xData.length; i++) {
+        if (xData[i] === x && series.options.data[i] && series.options.data[i].drilldown) {
+          ret.push(points ? points[i] : true);
+          break;
+        }
+      }
     });
-    p.call(this);
-  });
+    return ret;
+  };
 }(Highcharts));
