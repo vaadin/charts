0e08983e1d045c07f8dbffed5516222e39863147
Guillermo Alvarez <guillermo@vaadin.com>
2019-04-03 15:33:19 +0300

    Remove unneeded workaround

--- addon/src/main/resources/com/vaadin/addon/charts/client/highcharts-drilldown-workarounds.js
+++ addon/src/main/resources/com/vaadin/addon/charts/client/highcharts-drilldown-workarounds.js
@@ -1,15 +1,5 @@
 
 (function(Highcharts) {
-
-  // Workaround for https://github.com/vaadin/charts/issues/460 
-  // from https://github.com/highcharts/highcharts/issues/5565#issuecomment-237712885
-  Highcharts.wrap(Highcharts.Chart.prototype, 'drillUp', function (p) {
-    Highcharts.each(this.xAxis, function (axis) {
-        axis.names = [];
-    });
-    p.call(this);
-  });
-
   // https://github.com/highcharts/highcharts/issues/5750
   // http://jsfiddle.net/4q8jyrvm/
   Highcharts.Axis.prototype.getDDPoints = function(x) {
