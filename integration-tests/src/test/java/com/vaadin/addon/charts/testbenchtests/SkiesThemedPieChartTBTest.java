1c25453cd887ffa466856108e6a06b9d3374d189
Guille <alvarezguille@users.noreply.github.com>
2021-07-14 14:57:28 +0300

    Ignore skies theme tests (#618)

--- integration-tests/src/test/java/com/vaadin/addon/charts/testbenchtests/SkiesThemedPieChartTBTest.java
+++ integration-tests/src/test/java/com/vaadin/addon/charts/testbenchtests/SkiesThemedPieChartTBTest.java
@@ -2,6 +2,9 @@ package com.vaadin.addon.charts.testbenchtests;
 
 import com.vaadin.addon.charts.examples.themes.SkiesThemedPieChart;
 
+import org.junit.Ignore;
+
+@Ignore("test view uses an image that is pointing to an URL that no longer works")
 public class SkiesThemedPieChartTBTest extends
         AbstractSimpleScreenShotTestBenchTest {
 
