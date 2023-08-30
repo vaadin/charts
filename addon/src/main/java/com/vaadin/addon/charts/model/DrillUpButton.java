6afd39c27a8a919ee5a1e7e4ea72e0e8de96a5a6
Anna Koskinen <anna@vaadin.com>
2023-08-30 20:53:55 +0300

    Updated headers and license information.
    Switched to use com.mycila.license-maven-plugin for the updating.
    Appended DevInstructions.md with usage instructions.

--- addon/src/main/java/com/vaadin/addon/charts/model/DrillUpButton.java
+++ addon/src/main/java/com/vaadin/addon/charts/model/DrillUpButton.java
@@ -1,21 +1,14 @@
-package com.vaadin.addon.charts.model;
-
 /*
- * #%L
- * Vaadin Charts
- * %%
- * Copyright (C) 2012 - 2015 Vaadin Ltd
- * %%
- * This program is available under Commercial Vaadin Add-On License 3.0
- * (CVALv3).
+ * Vaadin Charts Addon
  *
- * See the file licensing.txt distributed with this software for more
- * information about licensing.
+ * Copyright (C) 2012-2023 Vaadin Ltd
  *
- * You should have received a copy of the CVALv3 along with this program.
- * If not, see <https://vaadin.com/license/cval-3>.
- * #L%
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
  */
+package com.vaadin.addon.charts.model;
 
  /**
  * Options for the drill up button that appears when drilling down on a series.
